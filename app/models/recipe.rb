abd7c5e65128b224f5c88ebde511415b4d22f668
Mauricio Labarca <mauriciolabarca4tw@gmail.com>
2015-09-26 19:45:35 -0300

    Fixes recipe model includes and env variables.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,10 @@
 class Recipe
+  include HTTParty
+
   #HTTParty params
-  hostport = ENV[’FOOD2FORK_SERVER_AND_PORT’] || ’www.food2fork.com’
-  base_uri 'http://#{hostport}/api'
-  default_params key: ENV['FOOD2FORK_KEY']
+  hostport = ENV["FOOD2FORK_SERVER_AND_PORT"] || 'www.food2fork.com'
+  base_uri "http://#{hostport}/api"
+  default_params key: ENV["FOOD2FORK_KEY"]
   format :json
   
   def self.for food
